0ef88c862ee7e0bcef6f4e2a87bd217791e4d0d6
Rafal Lalik <RafalLalik@ph.tum.de>
2016-01-05 00:37:51 +0100

    License change

--- sympy2cpp.cpp
+++ sympy2cpp.cpp
@@ -1,6 +1,6 @@
 /*
  * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2013  Rafał Lalik <rafal.lalik@ph.tum.de>
+ * Copyright (C) 2016  Rafał Lalik <rafal.lalik@ph.tum.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 
 #include <cstdlib>
 #include <iostream>
